21128f3b8e651920acf230831c0819e9169d825c
chrisngili <ngilichristopher@gmail.com>
2025-03-12 11:38:11 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name){
+    return drivers.filter((driver) => driver.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(drivers, name){
+    return drivers.filter((driver) => driver.indexOf(name) === 0 )
+}
+
+const driver = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+  function matchName(driver, name){
+    return driver.filter((dere) => dere.name===name)
+  }
